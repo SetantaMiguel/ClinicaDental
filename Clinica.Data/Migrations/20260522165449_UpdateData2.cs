8809bdabcc0d564764ad36949c85ccbf141238e6
Miguel Sanchez <miasango2@hotmail.com>
2026-06-05 10:14:13 -0600

    CARGA DE POSTGRESQL

---
+++ Clinica.Data/Migrations/20260522165449_UpdateData2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clinica.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateData2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Identificacion",
+                table: "Pacientes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Identificacion",
+                table: "Pacientes");
+        }
+    }
+}
